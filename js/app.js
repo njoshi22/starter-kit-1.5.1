dc047014d942ddf2d005efbdfd5f22452043fe36
njoshi22 <njoshi22@icloud.com>
2014-05-27 21:27:46 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -5,10 +5,18 @@ App.ApplicationAdapter = DS.RESTAdapter.extend({
 	host: 'http://node-api.azurewebsites.net'
 });
 
+/* App.Store = DS.Store.extend({
+	adapter: DS.RESTAdapter.create({
+		//namespace: 'api/v1',
+		url: 'http://node-api.azurewebsites.net/api/v1'
+	})
+}); */
+
 DS.RESTAdapter.reopen({
 	namespace: 'api/v1',
 	host: 'http://node-api.azurewebsites.net'
 });
+
 
 App.Router.map(function() {
   this.resource('tests', {path: '/tests'});
--- js/components.js
+++ js/components.js
@@ -1,7 +1,24 @@
-// App.BlogPostComponent = Ember.Component.extend({
-// 	actions: {
-// 		toggleBody: function() {
-// 			this.toggleProperty('viewBody');
-// 		}
-// 	}
-// });
+App.WhStrComponent = Ember.Component.extend({
+	init: function() {
+		this._super();
+		this.set('isExpanded',true);
+		this.set('status','Not set');
+	},
+	isExpanded: true,
+	status: 'Not set',
+	actions: {
+		toggleDetails: function() {
+			//this.toggleProperty('isExpanded');
+			/* var warehouse = store.createRecord('warehouse',{
+				whid: 'test',
+				name: 'test WH',
+				location: 'UN-MA'
+			}).save().then(function() {
+				this.set('status','saved');
+			}).catch(function() {
+				this.set('status','failed');
+			}); */
+			this.set('status','Yay!');
+		}
+	}
+});
