4d6b5f61ae1483eda7a3d889fc7494579644790a
njoshi22 <njoshi22@icloud.com>
2014-10-30 22:25:12 +0530

    asdfasfd

--- ExpenseApp/js/app.js
+++ ExpenseApp/js/app.js
@@ -5,7 +5,6 @@ App.Store = DS.Store.extend({
         host: 'http://neilapi.azurewebsites.net',
         namespace: 'api',
         serializer: App.ApplicationSerializer
-    })
 });
 
 App.ApplicationSerializer = DS.RESTSerializer.extend({
@@ -19,19 +18,34 @@ App.Router.map(function() {
 App.IndexRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('expense');
+    },
+    actions: {
+        loading: function(transition) {
+            var el = $('.spinner');
+            el.show();
+            transition.then(function() {
+                el.hide();
+            });
+            return true;
+        }
     }
 });
 
+App.IndexController = Ember.ArrayController.extend({
+    actions: {
+        saveExpense: function() {
+            var name = this.get('name');
+            var newExpense = this.get('store').createRecord('expense',{
+                name: "Boo",
+                amount: 20
+            });
+            newExpense.save();
+            this.store.push('expense',newExpense);
+        }
+    }
+});
 
 App.Expense = DS.Model.extend({
     name: DS.attr('string'),
     amount: DS.attr('number')
 });
-
-//App.Expense.FIXTURES = [
-//        {
-//            name: "Expense1",
-//            amount: 18,
-//            id: 1
-//        }
-//    ];
