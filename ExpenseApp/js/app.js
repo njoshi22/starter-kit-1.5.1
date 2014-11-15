bc5aec3469bfb03d5893a69e02d6e817b991f9e5
njoshi22 <njoshi22@icloud.com>
2014-11-15 23:09:32 +0530

    good working thing.

--- ExpenseApp/js/app.js
+++ ExpenseApp/js/app.js
@@ -1,19 +1,31 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+    LOG_TRANSITIONS: true
+});
 
-App.ApplicationStore = DS.Store.extend({
-    adapter: DS.RESTAdapter.extend({
-        host: 'http://neilapi.azurewebsites.net',
-        namespace: 'api',
-        serializer: App.ApplicationSerializer
-    })
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+    host: 'http://neilapi.azurewebsites.net',
+    namespace: 'api',
+    serializer: App.ApplicationSerializer
 });
 
 App.ApplicationSerializer = DS.RESTSerializer.extend({
     primaryKey: '_id'
 });
 
+// App.ApplicationAdapter = DS.FixtureAdapter();
+
+App.ApplicationController = Ember.Controller.extend({
+    currentPathDidChange: function() {
+        Ember.run.schedule('afterRender',this,function() {
+            console.log('route changed');
+            this.store.all('expense').update();
+        });
+    }.observes('currentPath')
+});
+
 App.Router.map(function() {
   // put your routes here
+  this.resource('charts');
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -38,29 +50,100 @@ App.IndexController = Ember.ArrayController.extend({
             var name = this.get('name');
             var newExpense = this.get('store').createRecord('expense',{
                 name: name,
-                amount: 20
+                amount: Math.floor(Math.random()*20*1.5)
             });
             newExpense.save();
-            // this.store.push('expense',newExpense);
+            this.set('name') = '';
+            // this.get('model').pushObject(newExpense);
+            // this.get('store').reload();
+            //this.store.update('expense',newExpense);
         },
         deleteExpense: function(expense) {
-            expense.deleteRecord();
-            expense.save();
-        },
-        sumExpenses: function() {
-            var expenses = this.store.find('expense');
-            var res = 0;
-            var amounts = expenses.mapBy('amount');
-            amounts.forEach(function(item){
-                res += item;
-            });
-
-            return res;
-        }.property('@each.amount')
-    }
+            // expense.deleteRecord();
+            // expense.save();
+            expense.destroyRecord();
+        }
+    },
+    expenseCount: function() {
+        return this.get('model.length');
+    }.property('@each')
 });
 
 App.Expense = DS.Model.extend({
     name: DS.attr('string'),
-    amount: DS.attr('number')
+    amount: DS.attr('number'),
+    label: function() {
+        return this.get('name');
+    }.property('name'),
+    value: function() {
+        return this.get('amount');
+    }.property('amount'),
+    group: function() {
+        return "expense";
+    }.property()
 });
+
+App.ChartsRoute = Ember.Route.extend({
+    // setupController: function(controller,model) {
+    //     this._super(controller,model);
+    //     var expenses = this.store.all('expense');
+    //     controller.set('model',expenses);
+    // }
+});
+
+App.ChartsController = Ember.Controller.extend({
+     getData: function() {
+        // this.store.unloadAll('expense');
+        var expenses = this.store.all('expense');
+        expenses.update();
+        var retarr = Ember.A();
+        expenses.forEach(function(expense) {
+            retarr.pushObject({
+                label: expense.get('name'),
+                value: expense.get('amount'),
+                group: 'expense'
+            });
+        });
+        return retarr;
+    }.property().volatile()
+});
+
+/* yayData: [
+    {
+        "label": "Equity",
+        "value": 12935781.176999997,
+        "group": "money"
+    },
+    {
+        "label": "Real Assets",
+        "value": 10475849.276172025,
+        "group": "money"
+    },
+    {
+        "label": "Fixed Income",
+        "value": 8231078.16438347,
+        "group": "money"
+    },
+    {
+        "label": "Cash & Cash Equivalent",
+        "value": 5403418.115000006,
+        "group": "money"
+    },
+    {
+        "label": "Hedge Fund",
+        "value": 1621341.246006786,
+        "group": "money"
+    },
+    {
+        "label": "Private Equity",
+        "value": 1574677.59,
+        "group": "money"
+    }
+  ] */
+
+  // App.Expense.FIXTURES = [
+  //   {
+  //       name: "Expense 1",
+  //       amount: 20
+  //   }
+  // ];
