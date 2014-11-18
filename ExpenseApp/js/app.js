2cb08490a0aef055379a65585f1f091e2a35f525
njoshi22 <njoshi22@icloud.com>
2014-11-18 22:58:02 +0530

    awesome v3

--- ExpenseApp/js/app.js
+++ ExpenseApp/js/app.js
@@ -18,7 +18,8 @@ App.Router.map(function() {
     });
 });
 
-// This gets rid of the # part of the app using HTMl5 history API
+// This gets rid of the # part of the app using HTMl5 history API.
+// DISABLED FOR LOCALLY VIEWING FILES. ENABLE FOR SERVER UPLOAD.
 App.Router.reopen({
     location: 'history',
     rootURL: '/ExpenseApp/'
